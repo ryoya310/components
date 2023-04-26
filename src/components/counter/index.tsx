ca85a17ea471e34eea4f317776fef6594b931104
Ryoya <109121569+ryoya310@users.noreply.github.com>
2023-04-26 11:30:38 +0900

    picker, counter

---
+++ src/components/counter/index.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react'
+
+import styles from './index.module.scss'
+
+import AddIcon from '@mui/icons-material/Add'
+import RemoveIcon from '@mui/icons-material/Remove'
+
+type Props = {
+  name: string
+  init?: number
+  step?: number
+  max?: number
+  min?: number
+  change?(no: number): void
+}
+export default function Counter(props: Props) {
+
+  const { name, init = 0, step = 1, max, min, change } = props
+  const [no, setNo] = React.useState(init)
+  const [intervalId, setIntervalId] = React.useState<NodeJS.Timeout | null>(null)
+
+  const getNo = (no: number) => {
+    let nextNo = no
+    if (max !== undefined && max <= nextNo) { nextNo = max }
+    if (min !== undefined && min >= nextNo) { nextNo = min }
+    return nextNo
+  }
+  const changeToggle = (no: number) => {
+    setNo(prevNo => getNo(prevNo + no))
+  }
+  const changeCounter = (no: number) => {
+    setNo(prevNo => getNo(no))
+  }
+
+  const handleMouseDown = (no: number) => {
+    const id = setInterval(() => {
+      changeToggle(no)
+    }, 100)
+    setIntervalId(id)
+  }
+
+  const handleMouseUp = () => {
+    if (intervalId !== null) {
+      clearInterval(intervalId)
+      setIntervalId(null)
+    }
+  }
+
+  React.useEffect(() => {
+    if (change) { change(no) }
+  }, [no, change])
+
+  return <>
+    <div className={styles.counters}>
+      <div className={styles.prev}>
+        <button
+          type='button'
+          onMouseDown={() => handleMouseDown(step * -1)}
+          onMouseUp={handleMouseUp}
+          onTouchStart={() => handleMouseDown(step * -1)}
+          onTouchEnd={handleMouseUp}
+        >
+          <RemoveIcon />
+        </button>
+      </div>
+      <div className={styles.count}>
+        <input
+          name={name}
+          type='number'
+          value={no}
+          max={max}
+          min={min}
+          onChange={(e) => changeCounter(Number(e.target.value))}
+        />
+      </div>
+      <div className={styles.next}>
+        <button
+          type='button'
+          onMouseDown={() => handleMouseDown(step * 1)}
+          onMouseUp={handleMouseUp}
+          onTouchStart={() => handleMouseDown(step * 1)}
+          onTouchEnd={handleMouseUp}
+        >
+          <AddIcon />
+        </button>
+      </div>
+    </div>
+  </>
+}
--- src/components/picker/index.tsx
+++ src/components/picker/index.tsx
@@ -3,21 +3,55 @@ import styles from './index.module.scss'
 import Loading from '../loading'
 import SearchIcon from '@mui/icons-material/Search'
 
+/**
+ * 0 -> field
+ * 1 -> type
+ * 2 -> default
+ * 3 -> input type
+ * 4 -> placeholder
+ * 5 -> select | option | radio | checkbox
+ */
+type RequestItem = [
+  string,
+  number | string,
+  string?,
+  string?,
+  {
+    id: number,
+    caption: string
+  }[]?
+]
 type Props = {
   url: string
   field?: [string, string][]
+  beforeRequest?: RequestItem[]
+  afterRequest?: RequestItem[]
   callback?(item: any): void
 }
 const Picker = (props: Props) => {
 
-  const { url, field = [['id', 'ID'], ['name', '名前']], callback } = props
+  const { url, field = [['id', 'ID'], ['name', '名前']], beforeRequest = [], afterRequest = [], callback } = props
 
   const listRef = React.useRef<HTMLDivElement>(null)
 
-  const [request, setRequest] = React.useState({
-    PageNo: 1,
-    SearchWord: ''
+  const initRequest: RequestItem[] = [
+    ['PageNo', 1],
+    ['SearchWord', '', 'text', '検索ワード'],
+  ]
+
+  const requests = [...beforeRequest, ...initRequest, ...afterRequest]
+
+  type RequestProps = {
+    [key: string]: number | string
+  }
+  const [request, setRequest] = React.useState<RequestProps>(() => {
+    const state: RequestProps = {}
+    requests.forEach(([field, defaultValue]) => {
+      state[field] = defaultValue
+    })
+    return state
   })
+
   const [list, setList] = React.useState<any[]>([])
   const [last, setLast] = React.useState(1)
   const [show, setShow] = React.useState<boolean | undefined>(undefined)
@@ -28,7 +62,7 @@ const Picker = (props: Props) => {
   }
 
   // リスト取得
-  const getList = async (url: string, request: any, type: string) => {
+  const getList = async (url: string, request: RequestProps, type: string) => {
     if (show !== undefined) {
       setShow(false)
     }
@@ -52,9 +86,58 @@ const Picker = (props: Props) => {
   }
 
   // 項目変更
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setRequest((prevState: any) => ({ ...prevState, [e.target.name]: e.target.value }))
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target
+    setRequest(prevState => ({ ...prevState, [name]: value }))
   }
+
+  const html = requests.map(([name, defaultValue, inputType, placeholder, options]) => {
+    if (!inputType) return
+    switch (inputType) {
+      case 'select':
+        if (Array.isArray(options)) {
+          return (
+            <select name={name} defaultValue={defaultValue} onChange={handleChange}>
+              <option value={''}>{placeholder}</option>
+              {options.map((option: { id: number, caption: string }) => (
+                <option key={option.id} value={option.id}>{option.caption}</option>
+              ))}
+            </select>
+          )
+        }
+        break
+      case 'radio':
+        if (Array.isArray(options)) {
+          return (
+            <div>
+              {options.map((option: { id: number, caption: string }) => (
+                <label key={option.id}>
+                  <input type='radio' name={name} value={option.id} onChange={handleChange} /> {option.caption}
+                </label>
+              ))}
+            </div>
+          )
+        }
+        break
+      case 'checkbox':
+        if (Array.isArray(options)) {
+          return (
+            <div>
+              {options.map((option: { id: number, caption: string }) => (
+                <label key={option.id}>
+                  <input type='checkbox' name={name} value={option.id} onChange={handleChange} /> {option.caption}
+                </label>
+              ))}
+            </div>
+          )
+        }
+        break
+      default:
+        return (
+          <input type={inputType ?? 'text'} name={name} defaultValue={defaultValue} placeholder={placeholder} onChange={handleChange} />
+        )
+    }
+  })
 
   // リスト絞込
   const search = () => {
@@ -62,7 +145,7 @@ const Picker = (props: Props) => {
     if (element) {
       element.scrollTop = 0
     }
-    setRequest((prevState: any) => ({...prevState, ['PageNo']: 1}))
+    setRequest((prevState: RequestProps) => ({...prevState, ['PageNo']: 1}))
     getList(url, {...request, PageNo: 1}, 'change')
   }
 
@@ -71,9 +154,9 @@ const Picker = (props: Props) => {
     function handleScroll() {
       const element = listRef.current
       if (element && element.scrollHeight - element.scrollTop <= element.clientHeight) {
-        if (last > request.PageNo) {
-          setRequest((prevState: any) => ({...prevState, ['PageNo']: prevState.PageNo + 1}))
-          getList(url, {...request, PageNo: request.PageNo + 1}, 'add')
+        if (+last > +request.PageNo) {
+          setRequest((prevState: RequestProps) => ({...prevState, ['PageNo']: +prevState.PageNo + 1}))
+          getList(url, {...request, PageNo: +request.PageNo + 1}, 'add')
         }
       }
     }
@@ -91,8 +174,12 @@ const Picker = (props: Props) => {
   return <>
     <div className={styles.picker}>
       <div className={styles.navi}>
-        <input name='SearchWord' placeholder='検索キーワード' onChange={handleChange} />
-        <button onClick={search}><SearchIcon /></button>
+        <div>
+          {html}
+        </div>
+        <div>
+          <button onClick={search}><SearchIcon /></button>
+        </div>
       </div>
       <div className={styles.contents}>
         <div className={styles.list} ref={listRef}>
@@ -105,7 +192,7 @@ const Picker = (props: Props) => {
                     <th key={arr[0]}>{arr[1]}</th>
                   ))
                 }
-                <th style={{width: 'auto'}}></th>
+                <th style={{width: '100%'}}></th>
               </tr>
             </thead>
             <tbody>
@@ -120,7 +207,7 @@ const Picker = (props: Props) => {
                       <td key={arr[0]}>{item[arr[0]]}</td>
                     ))
                   }
-                  <td style={{width: 'auto'}}></td>
+                  <td style={{width: '100%'}}></td>
                 </tr>
               ))
             }
